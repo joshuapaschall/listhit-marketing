0b5e41a06270ecfbbe2d0d1e853fc5ba5c6f71e6
joshuapaschall <joshsellshouses@gmail.com>
2026-01-04 10:39:51 -0500

    Delete listhit-marketing-main-2 directory

--- listhit-marketing-main-2/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function LoginRedirectPage() {
-  redirect("https://app.listhit.io");
-}
--- listhit-marketing-main-2/turnstile.d.ts
+++
@@ -1,9 +0,0 @@
-export {};
-
-declare global {
-  interface Window {
-    turnstile?: {
-      reset(widgetId?: string): void;
-    };
-  }
-}
